e3c0550121b4923571f43cdb17bbae853c522118
hkb1414 <43580459+hkbonestroo@users.noreply.github.com>
2022-05-11 23:14:16 -0400

    sort of done
    Things that still need to be edited:
    - legend in pdf??
    - question helpers added
    - markdown / about pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 //load map
 var map = L.map('map', {
   center: [36.09, -79.885],
@@ -25,10 +26,6 @@ var map2 = L.map('map2', {
   zoomControl: false
 });
 
-//add zoom control top right
-L.control.zoom({
-  position:'topright'
-}).addTo(map2);
 
 var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -37,6 +34,23 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{
   maxZoom: 20,
   ext: 'png'
 }).addTo(map2);
+
+//control maps together
+function changeMapState(src,tgt){
+  tgt.setZoom(src.getZoom());
+  tgt.panTo(src.getCenter());
+}
+
+map.on('moveend', function(e) {
+  changeMapState(map, map2);
+
+});
+
+// map2.on('moveend', function(e) {
+//   changeMapState(map2, map);
+// });
+
+
 
 //FIRE STATIONS
 //load fire stations
@@ -49,7 +63,9 @@ for (var i = 0; i < features.length; i = i + 1 ) {
 
 //add and remove marker functions
 var addToMap = function(){markers.forEach(function(marker){marker.addTo(map)})}
+var addToMap2 = function(){markers2.forEach(function(marker){marker.addTo(map2)})}
 var removeMap = function(){markers.forEach(function(marker){map.removeLayer(marker)})}
+var removeMap2 = function(){markers2.forEach(function(marker){map2.removeLayer(marker)})}
 
 //create markers
 var markers =[];
@@ -58,14 +74,24 @@ markers[i] =   L.marker([element[2].coordinates[1], element[2].coordinates[0]],{
  iconUrl: 'images/fire-station.png', iconSize: [25, 25]})}).bindPopup("Name: "+ element[1].Name).addTo(map))
  removeMap()
 
+ var markers2 =[];
+ newArray.forEach((element, i) => 
+ markers2[i] =   L.marker([element[2].coordinates[1], element[2].coordinates[0]],{icon: L.icon({  
+  iconUrl: 'images/fire-station.png', iconSize: [25, 25]})}).bindPopup("Name: "+ element[1].Name).addTo(map2))
+  removeMap2()
+ 
+
 // hide and show fire station markers
 function showFireStations() {
 if(map.hasLayer(markers[0])==true){
   removeMap()
+  removeMap2()
 }
 else{
   addToMap()
+  addToMap2()
 }}
+
 
 //FIRE DISTRICTS
 //neutral style
@@ -81,6 +107,16 @@ function styleb(feature) {
 
 //hide and show fire districts
 var FireDistrictsLayer= L.geoJSON(FireDistricts, {style: styleb,
+  onEachFeature: function (feature, layer) {
+    layer.bindPopup('District:'+feature.properties.DistName+"<br>"+'Budget: '+feature.properties.Budget+"<br>"+
+    'Average Busy Minutes: '+feature.properties.AvgBusyMin+"<br>"+'Annual Busy Hours: '+feature.properties.AnBusyHour+"<br>"+
+    "Total Staff: "+feature.properties.TotStaff+"<br>"+
+    "Percent Career: "+feature.properties.PerCareer+"<br>"+
+    "Average Predicted Risk: "+feature.properties.RiskMean + "<br>"+
+    "Average Predicted Risk (normalized): "+feature.properties.RiskMeanNo);
+  }
+});
+var FireDistrictsLayer2= L.geoJSON(FireDistricts, {style: styleb,
   onEachFeature: function (feature, layer) {
     layer.bindPopup('District:'+feature.properties.DistName+"<br>"+'Budget: '+feature.properties.Budget+"<br>"+
     'Average Busy Minutes: '+feature.properties.AvgBusyMin+"<br>"+'Annual Busy Hours: '+feature.properties.AnBusyHour+"<br>"+
@@ -112,7 +148,24 @@ else{
     }
   });
   map.addLayer(FireDistrictsLayer)
-}}
+}
+if(map2.hasLayer(FireDistrictsLayer2)==true){
+  map2.removeLayer(FireDistrictsLayer2)
+}
+else{
+  FireDistrictsLayer2= L.geoJSON(FireDistricts, {style: styleb,
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup('District:'+feature.properties.DistName+"<br>"+'Budget: '+feature.properties.Budget+"<br>"+
+      'Average Busy Minutes: '+feature.properties.AvgBusyMin+"<br>"+'Annual Busy Hours: '+feature.properties.AnBusyHour+"<br>"+
+      "Total Staff: "+feature.properties.TotStaff+"<br>"+
+      "Percent Career: "+feature.properties.PerCareer+"<br>"+
+      "Median Predicted Risk: "+feature.properties.RiskMed+ "<br>"+
+      "Median Predicted Risk (normalized): "+feature.properties.RiskMedNo);
+    }
+  });
+  map2.addLayer(FireDistrictsLayer2)
+}
+}
 
 //BUDGET
 function getColorBUDGET(d) {
@@ -136,7 +189,6 @@ function styleBUDGET(feature) {
 
 function Budget() {
   if(map.hasLayer(FireDistrictsLayer)==true){
- 
     map.removeLayer(FireDistrictsLayer)
     FireDistrictsLayer= L.geoJSON(FireDistricts, {style: styleBUDGET,
       onEachFeature: function (feature, layer) {
@@ -170,6 +222,15 @@ function Budget() {
     x.style.display = 'block';
     x2 = document.getElementById("table2");
     x2.style.display = 'block';
+  }
+  if(map2.hasLayer(FireDistrictsLayer2)==true){
+    map2.removeLayer(FireDistrictsLayer2)
+    FireDistrictsLayer2= L.geoJSON(FireDistricts, {style: styleBUDGET,
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup('District:'+feature.properties.DistName+"<br>"+'Budget: '+feature.properties.Budget);
+      }
+    });
+    map2.addLayer(FireDistrictsLayer2)
   }}
 
 
@@ -195,7 +256,6 @@ function styleABM(feature) {
 
 function AverageBusy() {
   if(map.hasLayer(FireDistrictsLayer)==true){
- 
     map.removeLayer(FireDistrictsLayer)
     FireDistrictsLayer= L.geoJSON(FireDistricts, {style: styleABM,
       onEachFeature: function (feature, layer) {
@@ -229,6 +289,15 @@ function AverageBusy() {
     x.style.display = 'block';
     x2 = document.getElementById("table2");
     x2.style.display = 'block';
+  }
+  if(map2.hasLayer(FireDistrictsLayer2)==true){
+    map2.removeLayer(FireDistrictsLayer2)
+    FireDistrictsLayer2= L.geoJSON(FireDistricts, {style: styleABM,
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup('District:'+feature.properties.DistName+"<br>"+'Average Busy Minutes: '+feature.properties.AvgBusyMin);
+      }
+    });
+    map2.addLayer(FireDistrictsLayer2)
   }}
 
   //ANNUAL BUSY HOUR
@@ -253,7 +322,6 @@ function getColorABH(d) {
  
  function AnnualBusy() {
    if(map.hasLayer(FireDistrictsLayer)==true){
-  
      map.removeLayer(FireDistrictsLayer)
      FireDistrictsLayer= L.geoJSON(FireDistricts, {style: styleABH,
        onEachFeature: function (feature, layer) {
@@ -287,6 +355,15 @@ function getColorABH(d) {
      x.style.display = 'block';
      x2 = document.getElementById("table2");
      x2.style.display = 'block';
+   }
+   if(map2.hasLayer(FireDistrictsLayer2)==true){
+    map2.removeLayer(FireDistrictsLayer2)
+     FireDistrictsLayer2= L.geoJSON(FireDistricts, {style: styleABH,
+       onEachFeature: function (feature, layer) {
+         layer.bindPopup('District:'+feature.properties.DistName+"<br>"+'Annual Busy Hours: '+feature.properties.AnBusyHour);
+       }
+     });
+     map2.addLayer(FireDistrictsLayer2)
    }}
 
  //TOTAL STAFF
@@ -311,7 +388,6 @@ function getColorABH(d) {
  
  function TotalStaff() {
    if(map.hasLayer(FireDistrictsLayer)==true){
-  
      map.removeLayer(FireDistrictsLayer)
      FireDistrictsLayer= L.geoJSON(FireDistricts, {style: styleTS,
        onEachFeature: function (feature, layer) {
@@ -345,7 +421,17 @@ function getColorABH(d) {
      x.style.display = 'block';
      x2 = document.getElementById("table2");
      x2.style.display = 'block';
-   }}
+   }
+   if(map2.hasLayer(FireDistrictsLayer2)==true){
+    map2.removeLayer(FireDistrictsLayer2)
+    FireDistrictsLayer2= L.geoJSON(FireDistricts, {style: styleTS,
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup('District:'+feature.properties.DistName+"<br>"+'Total Staff: '+feature.properties.TotStaff);
+      }
+    });
+    map2.addLayer(FireDistrictsLayer2)
+  }
+  }
 
 
 //PERCAREER
@@ -370,7 +456,6 @@ function getColorPC(d) {
  
  function PercentCareer() {
    if(map.hasLayer(FireDistrictsLayer)==true){
-  
      map.removeLayer(FireDistrictsLayer)
      FireDistrictsLayer= L.geoJSON(FireDistricts, {style: stylePC,
        onEachFeature: function (feature, layer) {
@@ -404,7 +489,16 @@ function getColorPC(d) {
      x.style.display = 'block';
      x2 = document.getElementById("table2");
      x2.style.display = 'block';
-   }}
+   }
+   if(map2.hasLayer(FireDistrictsLayer2)==true){
+    map2.removeLayer(FireDistrictsLayer2)
+    FireDistrictsLayer2= L.geoJSON(FireDistricts, {style: stylePC,
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup('District:'+feature.properties.DistName+"<br>"+'Percent Career Staff: '+feature.properties.PerCareer);
+      }
+    });
+    map2.addLayer(FireDistrictsLayer2)
+  }}
 
  
 
@@ -432,6 +526,7 @@ function style(feature) {
 
 //map RiskNet
 var RiskNetLayer=L.geoJson(RiskNet, {style: style});
+var RiskNetLayer2=L.geoJson(RiskNet, {style: style});
 
 // RiskNet.setStyle({color: "#f94144"});
 function showRiskNet() {
@@ -447,7 +542,15 @@ else{
   y.style.display = 'block';
   RiskNetLayer=L.geoJson(RiskNet, {style: style});
   map.addLayer(RiskNetLayer)
-}}
+}
+if(map2.hasLayer(RiskNetLayer2)==true){
+  map2.removeLayer(RiskNetLayer2);
+}
+else{
+  RiskNetLayer2=L.geoJson(RiskNet, {style: style});
+  map2.addLayer(RiskNetLayer2)
+}
+}
 
 //NORMALIZED RISK
 
@@ -472,7 +575,6 @@ function styleNR(feature) {
 
  function Normalize() {
    if(map.hasLayer(RiskNetLayer)==true){
-  
      map.removeLayer(RiskNetLayer)
      RiskNetLayer= L.geoJSON(RiskNet, {style: styleNR});
      map.addLayer(RiskNetLayer)
@@ -480,7 +582,12 @@ function styleNR(feature) {
      y = document.getElementById("legend-risk");
      x.style.display = 'none';
      y.style.display = 'block';
-   }}
+   }
+   if(map2.hasLayer(RiskNetLayer2)==true){
+    map2.removeLayer(RiskNetLayer2)
+    RiskNetLayer2= L.geoJSON(RiskNet, {style: styleNR});
+    map2.addLayer(RiskNetLayer2);
+  }}
 
 //show about page 
 function showAbout() {
@@ -571,118 +678,141 @@ function zoom1() {
   $('.district-name').text("Alamance Community")
   zoom =12
   map.flyTo([36.02022, -79.70769],zoom);
+  map2.flyTo([36.02022, -79.70769],zoom);
 }
 function zoom2() {
   $('.district-name').text("Climax")
   zoom =12
   map.flyTo([35.92295, -79.71042],zoom);
+  map2.flyTo([35.92295, -79.71042],zoom);
 }
 function zoom3() {
   $('.district-name').text("Colfax")
   zoom =12
   map.flyTo([36.1024, -80.02234],zoom);
+  map2.flyTo([36.1024, -80.02234],zoom);
 }
 function zoom4() {
   $('.district-name').text("District 13")
   zoom =12
   map.flyTo([36.16105, -79.74041],zoom);
+  map2.flyTo([36.16105, -79.74041],zoom);
 }
 function zoom5() {
   $('.district-name').text("District 28")
   zoom =12
   map.flyTo([36.15991, -79.57037],zoom);
+  map2.flyTo([36.15991, -79.57037],zoom);
 }
 function zoom6() {
   $('.district-name').text("Gibsonville")
   zoom =12
   map.flyTo([36.10716, -79.54003],zoom);
+  map2.flyTo([36.10716, -79.54003],zoom);
 }
 function zoom7() {
   $('.district-name').text("Guil-Rand")
   zoom =12
   map.flyTo([35.92944, -79.93953],zoom);
+  map2.flyTo([35.92944, -79.93953],zoom);
 }
 function zoom8() {
   $('.district-name').text("High Point")
   zoom =12
   map.flyTo([35.9828, -80.00133],zoom);
+  map2.flyTo([35.9828, -80.00133],zoom);
 }
 function zoom9() {
   $('.district-name').text("Julian")
   zoom =12
   map.flyTo([35.91943, -79.63466],zoom);
+  map2.flyTo([35.91943, -79.63466],zoom);
 }
 function zoom10() {
   $('.district-name').text("Kernersville")
   zoom =12
   map.flyTo([36.10932, -80.02858],zoom);
+  map2.flyTo([36.10932, -80.02858],zoom);
 }
 function zoom11() {
   $('.district-name').text("Kimesville")
   zoom =12
   map.flyTo([35.93282, -79.57655],zoom);
+  map2.flyTo([35.93282, -79.57655],zoom);
 }
 function zoom12() {
   $('.district-name').text("McLeansville")
   zoom =12
   map.flyTo([36.12385, -79.64453],zoom);
+  map2.flyTo([36.12385, -79.64453],zoom);
 }
 function zoom13() {
   $('.district-name').text("Mt. Hope")
   zoom =12
   map.flyTo([36.00037, -79.59946],zoom);
+  map2.flyTo([36.00037, -79.59946],zoom);
 }
 function zoom14() {
   $('.district-name').text("NE Guilford")
   zoom =12
   map.flyTo([36.22154, -79.67172],zoom);
+  map2.flyTo([36.22154, -79.67172],zoom);
 }
 function zoom15() {
   $('.district-name').text("Oak Ridge")
   zoom =12
   map.flyTo([36.1735, -79.99428],zoom);
+  map2.flyTo([36.1735, -79.99428],zoom);
 }
 function zoom16() {
   $('.district-name').text("Piedmont Triad Ambulance and Rescue")
   zoom =12
   map.flyTo([36.10033, -79.94108],zoom);
+  map2.flyTo([36.10033, -79.94108],zoom);
 }
 function zoom17() {
   $('.district-name').text("Pinecroft-Sedgefield")
   zoom =12
   map.flyTo([35.9637, -79.87617],zoom);
+  map2.flyTo([35.9637, -79.87617],zoom);
 }
 function zoom18() {
   $('.district-name').text("Pleasant Garden")
   zoom =12
   map.flyTo([35.96092, -79.76768],zoom);
+  map2.flyTo([35.96092, -79.76768],zoom);
 }
 function zoom19() {
   $('.district-name').text("Southeast")
   zoom =12
   map.flyTo([35.95481, -79.66331],zoom);
+  map2.flyTo([35.95481, -79.66331],zoom);
 }
 
 function zoom20() {
   $('.district-name').text("Stokesdale")
   zoom =12
   map.flyTo([36.24147, -79.98476],zoom);
+  map2.flyTo([36.24147, -79.98476],zoom);
 }
 function zoom21() {
   $('.district-name').text("Summerfield")
   zoom =12
   map.flyTo([36.22486, -79.87496],zoom);
+  map2.flyTo([36.22486, -79.87496],zoom);
 }
 function zoom22() {
   $('.district-name').text("Whitsett")
   zoom =12
   map.flyTo([36.07571, -79.57812],zoom);
+  map2.flyTo([36.07571, -79.57812],zoom);
 }
 
 function greensboro(){
   $('.district-name').text("Greensboro")
   zoom =12
   map.flyTo([36.087231, -79.833755],zoom);
+  map2.flyTo([36.087231, -79.833755],zoom);
 }
 
 //filter
@@ -711,3 +841,4 @@ function filterFunction3() {
 function myFunction3() {
   document.getElementById("myDropdown3").classList.toggle("show");
 }
+
